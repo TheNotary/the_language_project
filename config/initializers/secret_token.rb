0bf7f7442861703581bcca5c32444d567bc0b702
TheNotary <no@mail.plz>
2012-11-16 16:00:39 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheLanguageProject::Application.config.secret_token = '75def8583275a862b624d3bd1f64188b1ca2fb554a5359accd8db934fa8513afc4a004fa90b6495bee6309556c725019b071af622cd3dfcc00d418ab8da0888b'
